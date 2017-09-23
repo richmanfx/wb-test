34246a8e252e37387d0c2a50c9f75fea191a15b1
zoer <richmanfx@yamdex.ru>
2017-09-23 13:55:22 +0300

    Готово! 2017.09.23 13:55

--- src/cmd/wb-test/main.go
+++ src/cmd/wb-test/main.go
@@ -6,62 +6,111 @@ import (
 	"os"
 	"net/http"
 	"io/ioutil"
-	"fmt"
+	"strings"
+	"sync"
 )
+
 
 func init() {
 	log.SetFlags(0)
 }
 
-// Other funcs, if any.
-func sendGetRequest(url string) {
 
-	response, err := http.Get(url)
-	//defer response.Body.Close()
+/* Обработать ошибку. Если ошибка, то выход из приложения с кодом */
+func errorHandling(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	//reader := strings.NewReader("body")
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Sprint(body)
-	_, err = os.Stdout.Write(body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 }
 
 
+/* Отправить GET-запрос, вернуть количество "Go" */
+func sendGetRequest(url string) int {
+
+	response, err := http.Get(url)
+	defer response.Body.Close()
+	errorHandling(err)
+
+	body, err := ioutil.ReadAll(response.Body)
+	errorHandling(err)
+
+	goStringCount := counterGoString(string(body))
+
+	//log.Printf("Горутина для %s отработала", url)		// Раскомментировать для наглядности!
+
+	log.Printf("Count for %s: %d", url, goStringCount)
+
+	return goStringCount
+}
+
+
+/* Подсчитать количество строк "Go" */
+func counterGoString(request string) int {
+
+	goStringCount := strings.Count(request, "Go")
+	return goStringCount
+}
+
+
+/* Подсчитать общее количество строк "Go" на всех страницах */
+func totalCounterGoString(bufferedChannel chan int, totalCount *int) {
+	for {
+		// Читать из канала
+		goStringCount, readOk := <- bufferedChannel
+
+		//log.Printf("readOk: %v", readOk)		// Раскомментировать для наглядности!
+
+		// Не пустой ли уже канал
+		if !readOk {
+			break
+		}
+
+		// Добавить считанное количество в общую сумму
+		*totalCount += goStringCount
+
+		//log.Printf("Промежуточное значение: %v", *totalCount)	// Раскомментировать для наглядности!
+	}
+}
+
+
+/* Главная */
 func main() {
 
-	// Initialization, if any.
+	parallelGoroutinesCount := 5		// Максимальное число одновременно работающих горутин
+	bufferedChannel	:= make(chan int, parallelGoroutinesCount)		// Буферизированный канал
+	var wg sync.WaitGroup 		// Текущее количество работающих горутин
+	total := 0			// Суммарное количество строк "Go" на всех страницах
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-
-	for scanner.Scan() {
-		// Do something with strings here.
-		log.Printf("String: %s\n", scanner.Text())
-		go sendGetRequest(scanner.Text())
-
-
-	}
+	scanner := bufio.NewScanner(os.Stdin)	// Из пайпа в Scanner
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
 
-	// Other code, if any.
+	// Бежать по URL-ам
+	for scanner.Scan() {
 
-	total := 0
+		// Ссылка из Сканера
+		link := scanner.Text()
 
-	// Other code, if any.
+		// Увеличить счётчик текущего количества работающих горутин
+		wg.Add(1)
 
-	var input string
-	fmt.Scanln(&input)
+		// Количество строк "Go" - в канал
+		go func() {
+			defer wg.Done()
+			bufferedChannel <- sendGetRequest(link)
+		}()
+		//log.Printf("Запустилась горутина для %s", link)		// Раскомментировать для наглядности!
+	}
+
+	// Закрыть канал когда все горутины отработают
+	go func () {
+		wg.Wait()
+		close(bufferedChannel)
+	}()
+
+	// Вычитывать из канала количество строк и суммировать
+	totalCounterGoString(bufferedChannel, &total)
+
 	log.Printf("Total: %v", total)
 }
